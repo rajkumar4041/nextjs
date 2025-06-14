bf46c310a7931bf21d3f9603e600822a1b9d5592
Rajkumar rathod <127109370+rajkumar4041@users.noreply.github.com>
2025-06-14 18:33:47 +0530

    feat:not found default page (#6)

--- src/app/lectures/[[...slugs]]/page.tsx
+++ src/app/lectures/[[...slugs]]/page.tsx
@@ -1,15 +1,17 @@
 import { log } from 'console';
+import NotFoundPage from '@/app/not-found';
 
 const Lectures = async ({ params }: { params: Promise<{ slugs: string[] }> }) => {
   const { slugs } = await params;
   log('Lectures slugs:', slugs);
 
-  if (slugs?.length === 0) {
-    return <div>No lectures found</div>;
-  }
-
   if (slugs?.length === 1) {
-    return <div>Lecture: {slugs[0]}</div>;
+    // return <div>Lecture: {slugs[0]}</div>;
+
+    return NotFoundPage();
+
+    // works fine
+    // return <NotFoundPage/>
   }
 
   if (slugs?.length === 2) {
