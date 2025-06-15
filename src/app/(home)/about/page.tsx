b0bed16f188d54bf4bf1c80454d464afb14c38cf
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2025-06-15 20:38:04 +0530

    feat:metadata

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from 'next';
+
+/**
+ * this metadata is only for this about page & it will override the global metadata
+ */
+export const metadata: Metadata = {
+  title: {
+    default: 'About',
+    // render title of active metadata at the %s position and append " | About" to it
+    template: '%s | About',
+  },
+  description: 'About page description',
+};
+
 const About = () => {
   return <div>about</div>;
 };
