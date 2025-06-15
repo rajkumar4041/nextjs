208e70a68bf66ea85d3800908f36f9cf2c07d7c0
Rajkumar rathod <127109370+rajkumar4041@users.noreply.github.com>
2025-06-15 20:38:26 +0530

    feat:metadata (#12)

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from "next";
+
+/**
+ * this metadata is only for this about page & it will override the global metadata
+ */
+export const metadata:Metadata = {
+  title: {
+    absolute: 'Contact',
+  },
+  description: 'Contact page description',
+};
+
+
 const Contact = () => {
   return <div>Contact</div>;
 };
