8ee8db4c52009dea961bb3c47c42a240757986b3
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2025-06-21 00:49:18 +0530

    feat(parallel-route):add sub-navigation

--- src/app/(home)/charts/@barChart/page.tsx
+++ src/app/(home)/charts/@barChart/page.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+
 const BarChart = () => {
-  return <div>BarChart</div>;
+  return (
+    <div className="flex flex-col">
+      BarChart
+      {/* sub navigation
+        `@folder` structure - parallel routes 
+
+        @file sub-nav-chart/page.tsx
+        @description this will be used to navigate to the sub navigation chart
+
+        @see - this parallel route act like small application
+            - where all the routes are independently working
+            - only the parent barChart will change
+            - this will not change the main layout and also other parallel routes
+
+      */}
+      <Link href={'/charts/sub-nav-chart'}>go to Sub Nav Chart</Link>
+    </div>
+  );
 };
 
 export default BarChart;
