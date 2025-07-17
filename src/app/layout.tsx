62215b2c129c80281cdb211793bedc38253aafed
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2025-07-17 23:49:39 +0530

    feat(clerk): add clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
-import StoreProvider from './StoreProvider';
+import { ClerkProvider } from "@clerk/nextjs";
+import StoreProvider from "./StoreProvider";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>
-        <StoreProvider>{children}</StoreProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <StoreProvider>{children}</StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (request: NextRequest) => {
   const response = NextResponse.next();
 
   //conditional way
-  if (response.cookies.get('name')) {
-    console.log('check cookie', response.cookies.get('name'));
+  if (response.cookies.get("name")) {
+    console.log("check cookie", response.cookies.get("name"));
   } else {
-    response.cookies.set('abc', '12345');
-    response.headers.set('my-header', 'my-val');
+    response.cookies.set("abc", "12345");
+    response.headers.set("my-header", "my-val");
 
     return response;
   }
 
-  if (request.nextUrl.pathname === '/about') {
+  if (request.nextUrl.pathname === "/about") {
     // console.log('hit');
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   // return NextResponse.redirect(new URL('/', request.url));
@@ -25,3 +25,16 @@ export const middleware = (request: NextRequest) => {
 // export const config = {
 //   matcher: '/about',
 // };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
