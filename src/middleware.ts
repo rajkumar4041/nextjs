680214e853eb01a9fbb11581f93c12516f03f4f8
Rajkumar Rathod <rajkumarrathod414@gmail.com>
2025-07-19 00:24:12 +0530

    feat(auth): clerk log out and log in system with protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const middleware = (request: NextRequest) => {
-  const response = NextResponse.next();
+// export const middleware = (request: NextRequest) => {
+//   const response = NextResponse.next();
 
-  //conditional way
-  if (response.cookies.get("name")) {
-    console.log("check cookie", response.cookies.get("name"));
-  } else {
-    response.cookies.set("abc", "12345");
-    response.headers.set("my-header", "my-val");
+//   //conditional way
+//   if (response.cookies.get("name")) {
+//     console.log("check cookie", response.cookies.get("name"));
+//   } else {
+//     response.cookies.set("abc", "12345");
+//     response.headers.set("my-header", "my-val");
 
-    return response;
-  }
+//     return response;
+//   }
 
-  if (request.nextUrl.pathname === "/about") {
-    // console.log('hit');
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (request.nextUrl.pathname === "/about") {
+//     // console.log('hit');
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  // return NextResponse.redirect(new URL('/', request.url));
-};
+//   // return NextResponse.redirect(new URL('/', request.url));
+// };
 
 //one of the way matcher
 // export const config = {
 //   matcher: '/about',
 // };
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/protected-route"]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
